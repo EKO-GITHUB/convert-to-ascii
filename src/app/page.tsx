4d4d252b3f80e4c8b34f7428c00bd3fb82ecc1b3
EKO <murat.tochiev@gmail.com>
2025-07-14 04:05:32 +0200

    Changes: - version: 0.0.13 -> 0.0.14 - added `ConvertToAscii` component as boilerplate for future functionality - updated `page.tsx` to include `ConvertToAscii` component within main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
+import ConvertToAscii from "@/components/features/ConvertToAscii";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col bg-gradient-to-br">
       <Header />
-      <div className={"grow"}>TEST</div>
+      <div className={"grow"}>
+        <ConvertToAscii />
+      </div>
       <Footer />
     </main>
   );
